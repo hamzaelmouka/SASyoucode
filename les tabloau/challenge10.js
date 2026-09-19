cff0161f71756cce3c29e04faae4fd2eb02ddcea
hamzaelmouka <hmzelmouka@gmail.com>
2026-09-19 19:18:05 +0200

    les challenge 10 de recherch un element

--- les tabloau/challenge10.js
+++ les tabloau/challenge10.js
@@ -9,11 +9,11 @@ let elemont=Number(prompt("entre un entie qui rochirch"))
 let cont=0;
 for(let i=0;i<tai;i++){
     if(tab[i]===elemont){
-        console.log("aloure le nombre qui rocherch il y a dans le tableau à l'indice : "+i );
+        console.log("aloure le nombre qui rocherch il y a dans le tableau a lindice : "+i );
         cont++;
          break;
     }
 }
 if(cont===0){
-    console.log("aloure le nombre qui rocherch il ya pas  dans le tableau à l'indice : ");
+    console.log("aloure le nombre qui rocherch il ya pas  dans le tableau  : ");
 }
