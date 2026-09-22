0adcec62be833cba3f980f83ae9387c859452eef
hamzaelmouka <hmzelmouka@gmail.com>
2026-09-22 13:52:12 +0200

    change le pour la reponse se le challeneg affich le nombre jusca 0

--- les fonction/challenge7.js
+++ les fonction/challenge7.js
@@ -1,11 +1,9 @@
 const prompt = require('prompt-sync')();
-function number(x) {
-let tab=[];
- for(i=0;i<=x;i++){
-    tab[i]=i;
-    }
-    return tab;
- }
+function number(x){
+ for(let i=x;i>=0;i--){
+console.log(i)
+}
+}
 
 let x=Number(prompt("entre un nembre pour affich tout les nombre jeusca le nombre :"));
-console.log("alour le numbre jusca "+x+"et :"+number(x));
+number(x);
