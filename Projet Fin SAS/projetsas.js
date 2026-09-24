309ce719d22b3dd99808ca32caf2b1b6ee22afc0
hamzaelmouka <hmzelmouka@gmail.com>
2026-09-25 00:36:54 +0100

    avensmonte de joudi pour projit sas

--- Projet Fin SAS/projetsas.js
+++ Projet Fin SAS/projetsas.js
@@ -63,9 +63,7 @@ function AjouterCandidat(){
     }if(cont1==0){
     let nom=prompt("entre le nom de candidant   :");
     let prenom=prompt("entre le prenom de candidant   :");
-    let partiPolitique=prompt(` Vous organisez une fete ?
-        pour oui entre le nom de parter
-        si non entre le nombre 1 : :`);
+    let partiPolitique=prompt(` entre le nom du parti (ou Independant) : `);
     let age=prompt("entre le age  :");
     cont++;
     let candidat = { 
@@ -81,44 +79,108 @@ function AjouterCandidat(){
            le condidants et ajouter
             `);
     }
+  
 }
 function AjouterPlusieursCandidats(){
     let pluseur=Number(prompt("entre le nombre de candidant pou ajour : "));
     for(i=0;i<pluseur;i++){
         AjouterCandidat();
     }
+     menu(); 
 }
 function AfficherListeCandidats(){
     console.log(`
         1. affichage simple 
         2. trier les candidats par nombre de votes
         3. afficher uniquement les candidats d'un parti politique spécifique.
-        Votre choix :`);
-    let choix=prompt(" ");
+         :`);
+    let choix=Number(prompt(" Votre choix : "));
     switch(choix){
         case 1 :
-            let i=0;
-            for(let cle of candidats){
+            for(let i=0;i<cont;i++){
                 console.log(`
-                    cin    : ${cle.cin}
-                    nom    : ${cle.nom}
-                    prenom : ${cle.cin}
-                    age    : ${cle.cin}
-                    `);i++;
+                    cin    : ${candidats[i].cin}
+                    nom    : ${candidats[i].nom}
+                    prenom : ${candidats[i].prenom}
+                    parti  : ${candidats[i].partiPolitique}
+                    age    : ${candidats[i].age}
+                    votes  : ${candidats[i].electeurs.length}
+                    `);
             }
             break;
         case 2 :
-            for(let cle of candidats){
+            let reserv;
+            for(let i=0;i<cont-1;i++){
+                for(let j=0;j<cont-1-i;j++){
+                    if(candidats[j].electeurs.length<candidats[j+1].electeurs.length){
+                        reserv=candidats[j];
+                        candidats[j]=candidats[j+1];
+                        candidats[j+1]=reserv;
+                    }
+
+                }
                 
             }
+            for(let i=0;i<cont; i++){
+                console.log(`
+                    cin    : ${candidats[i].cin}
+                    nom    : ${candidats[i].nom}
+                    prenom : ${candidats[i].prenom}
+                    parti  : ${candidats[i].partiPolitique}
+                    age    : ${candidats[i].age}
+                    votes  : ${candidats[i].electeurs.length}
+                    `);
+            }
+            break;
+        case 3 :
+            let parti=prompt("entre le nom de parti")
+            for(let i=0;i<cont;i++){
+                if(candidats[i].partiPolitique==parti){
+                    console.log(`
+                        cin    : ${candidats[i].cin}
+                        nom    : ${candidats[i].nom}
+                        prenom : ${candidats[i].prenom}
+                        parti  : ${candidats[i].partiPolitique}
+                        age    : ${candidats[i].age}
+                        votes  : ${candidats[i].electeurs.length}
+                        `);
+                }
+            }
+            break;
+        default :
+        console.log("choix pas valide");
     }
-
+menu();
 
 }
 function VoterCandidat(){
-
+    let cinElecteur=prompt("entre le cin  :");
+    for(let i=0;i<cont;i++){
+        for(let j=0;j<candidats[i].electeurs.length;j++){
+            if(candidats[i].electeurs[j]==cinElecteur){
+                console.log("vous deja vote");
+                return menu();
+            }
+        }
+    }
+    let cinCandidat=prompt("entre le cin de candidat :");
+    for(let i=0;i<cont;i++){
+        if(candidats[i].cin==cinCandidat){
+            candidats[i].electeurs.push(cinElecteur);
+            console.log("vote enregistre");
+            return menu();
+        }
+    }
+    console.log("candidat introvable ");
+    menu();
 }
 function ModifierleCandidat(){
+    let cin=prompt("entre le cin du candidat : ");
+    for(let i=0;i<cont;i++){
+        if(candidats[i].cin==cin){
+            
+        }
+    }
 
 }
 function SupprimerCandidat(){
